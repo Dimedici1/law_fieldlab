933fdca7d5cb09bc29e2c38e02e7fa6c26711cd3
Dimedici1 <105723893+Dimedici1@users.noreply.github.com>
2023-11-24 12:19:38 +0000

    Update chatbot_gradio.py

--- run_model_files/examples/chatbot_gradio.py
+++ run_model_files/examples/chatbot_gradio.py
@@ -77,7 +77,7 @@ class ChatbotArguments:
     prompt_structure: Optional[str] = field(
         default=f"""<s>[INST] <<SYS>>
     Use the following pieces of context to answer the question at the end. If the questions refers to previous messages use the history and context to answer the question.
-    If you don't know the answer, just say that you don't know, don't try to make up an answer. End every message with \n\n\n\n 
+    If you don't know the answer, just say that you don't know, don't try to make up an answer. Always end your message with: \n\n\n\n 
     History: {{input_text}}
     Context: {{data}}
     <</SYS>>
