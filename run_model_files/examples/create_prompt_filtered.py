dad35225d501d05b7cb3e81b2a788f1aaefa803e
Dimedici1 <105723893+Dimedici1@users.noreply.github.com>
2023-12-09 19:11:09 +0000

    Create create_prompt_filtered.py

---
+++ run_model_files/examples/create_prompt_filtered.py
@@ -0,0 +1,42 @@
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.document_transformers import (
+    LongContextReorder,
+)
+from pathlib import Path
+import pandas as pd
+
+def rag(query, number_results, df, idx):
+    persist_directory = str(Path.home()) + "/law_fieldlab/create_database/database"
+    embeddings_function = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    
+    vector_db = Chroma(persist_directory=persist_directory, embedding_function=embeddings_function)
+    
+    #Get the correct paper
+    print(df["Paper"][idx])
+    filter_source = df["Paper"][idx]
+    retriever = vector_db.as_retriever(search_type="mmr", search_kwargs={"k": number_results, "filter":{"title": filter_source}})
+    
+    # Get relevant documents ordered by relevance score
+    query = query.lower().strip()
+    docs = retriever.get_relevant_documents(query)
+
+    # Reorder the documents
+    reordering = LongContextReorder()
+    docs = reordering.transform_documents(docs)
+
+    results = ""
+    all_sources = ""
+    for doc in docs:
+        source = doc.metadata["source"]
+        info = doc.page_content
+        info = info.lower().strip()
+        results += "### " + info + " (Source: " + source + ")"
+        results += " ###"
+    print(results)
+    return results
+
+def get_data(query: str, all_df, idx):
+    number_results = 4
+    results = rag(query, number_results, all_df, idx)
+    return results
