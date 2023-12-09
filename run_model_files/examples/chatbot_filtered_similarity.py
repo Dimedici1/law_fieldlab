84c0cb3830add3ed0f33db81797543a6487701d3
Dimedici1 <105723893+Dimedici1@users.noreply.github.com>
2023-12-09 19:27:55 +0000

    Update chatbot_filtered_similarity.py

--- run_model_files/examples/chatbot_filtered_similarity.py
+++ run_model_files/examples/chatbot_filtered_similarity.py
@@ -27,7 +27,7 @@ from create_prompt_filtered_mmr import get_data
 from pathlib import Path
 
 testing_data_path = str(Path.home()) + "/law_fieldlab/Testing/Master_Thesis_Questions.csv"
-saving_output_path = str(Path.home()) + "/law_fieldlab/Testing/filtered_mmr_responses.csv"
+saving_output_path = str(Path.home()) + "/law_fieldlab/Testing/filtered_similarity_responses.csv"
 
 logging.disable(logging.ERROR)
 warnings.filterwarnings("ignore")
