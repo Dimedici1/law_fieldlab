3c5aa53f0d172bec584bbaa9b337b1e84dd179c3
Dimedici1 <105723893+Dimedici1@users.noreply.github.com>
2023-12-10 23:24:23 +0000

    Create create_prompt_rag.py

---
+++ run_model_files/examples/self_consistency/create_prompt_rag.py
@@ -0,0 +1,36 @@
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.document_transformers import (
+    LongContextReorder,
+)
+from pathlib import Path
+
+def rag(query, number_results, similarity):
+    persist_directory = str(Path.home()) + "/law_fieldlab/create_database/database"
+    embeddings_function = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    
+    vector_db = Chroma(persist_directory=persist_directory, embedding_function=embeddings_function)
+    retriever = vector_db.as_retriever(search_type=similarity, search_kwargs={"k": number_results})
+    
+    # Get relevant documents ordered by relevance score
+    query = query.lower().strip()
+    docs = retriever.get_relevant_documents(query)
+
+    # Reorder the documents
+    reordering = LongContextReorder()
+    docs = reordering.transform_documents(docs)
+
+    results = ""
+    all_sources = ""
+    for doc in docs:
+        source = doc.metadata["source"]
+        info = doc.page_content
+        info = info.lower().strip()
+        results += "### " + info + " (Source: " + source + ")"
+        results += " ###"
+
+    return results
+
+def get_data(query, number_results, similarity):
+    results = rag(query, number_results, similarity)
+    return results
